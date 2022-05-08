2efed8022fa7a5418a2e01453903506228b2440b
Jānis Rubans <janis.rubans@cube.lv>
2022-05-08 22:35:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Installation
+
+#### Open your project directory
+```bash
+cd illustration3
+```
+
+#### Install npm dependencies by running
+```bash
+npm install
+```
+
+#### Run build script
+```bash
+npm run prod
+```
---
+++ src/php/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SVG Illustration</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+
+<div class="illustration-container">
+<div class="illustration">
+    <div class="text-container text-1"><div><span>Raw json-rpc</span></div></div>
+    <div class="text-container text-2"><div><span>Block details</span></div></div>
+    <div class="text-container text-3"><div><span>Blockchain events</span></div></div>
+    <div class="text-container text-4"><div><span>Assets</span></div></div>
+    <div class="text-container text-5"><div><span>Transaction details</span></div></div>
+    <div class="text-container text-6"><div><span>Tokens</span></div></div>
+    <div class="text-container text-7"><div><span>Create transactions</span></div></div>
+    <div class="text-container text-8"><div><span>Wallet details</span></div></div>
+    <div class="text-container text-9"><div><span>Address details</span></div></div>
+    <div class="text-container text-10"><div><span>Fees</span></div></div>
+    <div class="text-container text-11"><div><span>Transactions</span></div></div>
+
+    <?php include('assets/svg/illustration2.svg'); ?>
+</div>
+</div>
+
+
+
+    
+</body>
+</html>
